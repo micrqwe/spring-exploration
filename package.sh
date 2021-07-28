0978f392e20358a31bbca6c6423b329d1985b74f
swx <micrqwe@163.com>
2021-07-28 19:57:22 +0800

    添加脚本

---
+++ package.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+./mvnw clean package -Dmaven.test.skip=true
+mv spring-exploration-gateway/target/*.jar ../spring-exploration-jar
+mv spring-exploration-consumer/target/*.jar ../spring-exploration-jar
