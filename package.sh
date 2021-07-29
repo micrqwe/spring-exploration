94ab3e2bb04656f283a3f433ba222a2c0940a3e6
shaowenxing <shaowenxing@dyx.cn>
2021-07-29 20:03:16 +0800

    x

--- package.sh
+++ package.sh
@@ -3,4 +3,4 @@ git pull
 ./mvnw clean package -Dmaven.test.skip=true
 mv spring-exploration-gateway/target/*.jar ../spring-exploration-jar
 mv spring-exploration-consumer/target/*.jar ../spring-exploration-jar
-e
+mv spring-exploration-file/target/*.jar ../spring-exploration-jar
