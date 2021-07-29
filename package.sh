2817fe0a25013a1ba867b50fe8d3b3010651f4e9
shaowenxing <shaowenxing@dyx.cn>
2021-07-29 20:03:24 +0800

    删除无效

--- package.sh
+++ package.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-git pull
 ./mvnw clean package -Dmaven.test.skip=true
 mv spring-exploration-gateway/target/*.jar ../spring-exploration-jar
 mv spring-exploration-consumer/target/*.jar ../spring-exploration-jar
