4d7a9e7baad700ab5f6d66561ff548121e1f4619
shaowenxing <shaowenxing@dyx.cn>
2021-08-01 07:36:05 +0800

    修正

--- spring-exploration-file/src/main/java/xyz/micrqwe/FileApplication.java
+++ spring-exploration-file/src/main/java/xyz/micrqwe/FileApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.*;
 @SpringBootApplication
 public class FileApplication {
     public static void main(String[] args) throws Exception {
+//        Destroy method 'close' on bean with name 'nacosServiceRegistry' threw an exception: java.lang.NullPointerException
         SpringApplication.run(FileApplication.class, args);
     }
 }
