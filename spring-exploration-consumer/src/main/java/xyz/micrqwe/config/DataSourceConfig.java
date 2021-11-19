e779fd49c80cd927f69a04f210695c632b2231d1
shaowenxing <shaowenxing@dyx.cn>
2021-11-19 11:42:46 +0800

    2021-11-19.11:42:46自动提交

--- spring-exploration-consumer/src/main/java/xyz/micrqwe/config/DataSourceConfig.java
+++ spring-exploration-consumer/src/main/java/xyz/micrqwe/config/DataSourceConfig.java
@@ -1,8 +1,11 @@
 package xyz.micrqwe.config;
 
+import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.core.MybatisConfiguration;
 import com.baomidou.mybatisplus.core.config.GlobalConfig;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import com.baomidou.mybatisplus.extension.spring.MybatisSqlSessionFactoryBean;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.shardingsphere.api.config.sharding.ShardingRuleConfiguration;
@@ -145,7 +148,12 @@ public class DataSourceConfig {
         manager.setDataSource(dataSource);
         return manager;
     }
-
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
 /*    @Bean
     public TransactionInterceptor txAdvice() {
         return TransactionBuilder.buildAdvice(transactionManager());
